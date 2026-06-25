0e835874415e9e55c58b5b02b22d0c5418aef6d9
Sauryaks0731 <287573010+Sauryaks0731@users.noreply.github.com>
2026-06-25 18:05:53 +0530

    Add files via upload

---
+++ DAY21/REVSTR.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    int i=0;
+
+    printf("Enter string: ");
+    scanf("%s",str);
+
+    while(str[i]!='\0')
+        i++;
+
+    for(i=i-1;i>=0;i--)
+        printf("%c",str[i]);
+
+    return 0;
+}
